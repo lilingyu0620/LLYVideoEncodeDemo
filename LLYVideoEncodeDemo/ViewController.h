ef41c0660af5146397109cad6605fe4a3e4296e5
lly <bjlilingyu@corp.netease.com>
2018-05-30 10:19:44 +0800

    first commit

---
+++ LLYVideoEncodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LLYVideoEncodeDemo
+//
+//  Created by lly on 2018/5/30.
+//  Copyright © 2018年 lly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
