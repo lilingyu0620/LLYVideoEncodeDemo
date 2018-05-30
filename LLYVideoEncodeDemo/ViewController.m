e87f8cf6203dfe1a50fe6f88d1e8d8f995a33053
lly <bjlilingyu@corp.netease.com>
2018-05-30 17:40:46 +0800

    video encode

--- LLYVideoEncodeDemo/ViewController.m
+++ LLYVideoEncodeDemo/ViewController.m
@@ -7,8 +7,22 @@
 //
 
 #import "ViewController.h"
+#import "ELPushStreamConfigeration.h"
 
-@interface ViewController ()
+#import "ELImageVideoCamera.h"
+#import "ELImageView.h"
+#import "ELImageVideoEncoder.h"
+
+@interface ViewController ()<ELVideoEncoderStatusDelegate>
+
+
+@property (nonatomic, strong) ELImageVideoCamera *videoCamera;
+@property (nonatomic, strong) ELImageView *imageView;
+@property (nonatomic, strong) ELImageVideoEncoder *videoEncode;
+
+@property (nonatomic, assign) BOOL started;
+
+@property (weak, nonatomic) IBOutlet UIButton *startBtn;
 
 @end
 
@@ -17,8 +31,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.started = NO;
+    
+    self.videoCamera = [[ELImageVideoCamera alloc]initWithFPS:kFrameRate];
+    [self.videoCamera startCapture];
+    
+    self.imageView = [[ELImageView alloc]initWithFrame:self.view.bounds];
+    [self.view insertSubview:self.imageView atIndex:0];
 }
 
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self.videoCamera addTarget:self.imageView];
+    
+}
+- (IBAction)startBtnClicked:(id)sender {
+    
+    if (self.started) {
+        [self.videoCamera removeTarget:self.videoEncode];
+        [self.videoEncode stopEncode];
+        self.videoEncode = nil;
+        self.started = NO;
+        [self.startBtn setTitle:@"开始录制" forState:UIControlStateNormal];
+    }
+    else{
+         self.videoEncode = [[ELImageVideoEncoder alloc] initWithFPS:kFrameRate maxBitRate:kMaxVideoBitRate avgBitRate:kAVGVideoBitRate encoderWidth:kDesiredWidth encoderHeight:kDesiredHeight encoderStatusDelegate:self];
+        [self.videoCamera addTarget:self.videoEncode];
+        self.started = YES;
+        [self.startBtn setTitle:@"结束录制" forState:UIControlStateNormal];
+    }
+    
+}
+
+- (void) onEncoderInitialFailed{}
+
+- (void) onEncoderEncodedFailed{}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
